179a1a2e610cec838343df5c51578d3d69d028ef
Brian Enrique Maldonado Estrella <14470214@itcampeche.edu.mx>
2017-12-04 12:24:13 -0600

    criminallntent: Muestra una lista simple de crimenes.

--- app/src/main/java/com/example/sandraestrella/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/sandraestrella/criminalintent/CrimeListFragment.java
@@ -10,6 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -46,40 +50,57 @@ public class CrimeListFragment extends Fragment{
 
 
         private class CrimeHolder extends RecyclerView.ViewHolder {
-            public TextView mTitleTextView;
+            private Crime mCrime;
+            private TextView mTitleTextView;
+            private TextView mDateTextView;
+            private CheckBox mSolvedCheckBox;
+
 
             public CrimeHolder(View itemView) {
                 super(itemView);
 
-                mTitleTextView = (TextView) itemView;
+                mTitleTextView = (TextView) itemView
+                        .findViewById(R.id.list_item_crime_title_text_view);
+                mDateTextView = (TextView) itemView
+                        .findViewById(R.id.list_item_crime_date_text_view);
+                mSolvedCheckBox = (CheckBox) itemView
+                        .findViewById(R.id.list_item_crime_solved_chech_box);
             }
+
+
+        public void bindCrime(Crime crime){
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getmTitle());
+            mDateTextView.setText(mCrime.getmDate().toString());
+            mSolvedCheckBox.setChecked(mCrime.ismSolved());
         }
 
-        private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
-            private List<Crime> mCrimes;
-            public CrimeAdapter(List<Crime> crimes){
-                mCrimes = crimes;
+            private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+                private List<Crime> mCrimes;
+
+                public CrimeAdapter(List<Crime> crimes){
+                    mCrimes = crimes;
+                }
+
+                @Override
+                public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                    LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+                    View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+                    return new CrimeHolder(view);
+                }
+
+                @Override
+                public void onBindViewHolder(CrimeHolder holder, int position) {
+                    Crime crime = mCrimes.get(position);
+                    holder.mTitleTextView.setText(crime.getmTitle());
+                }
+
+                @Override
+                public int getItemCount() {
+                    return mCrimes.size();
+                }
             }
 
-            @Override
-            public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-                View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
-
-                return new CrimeHolder(view);
-            }
-
-            @Override
-            public void onBindViewHolder(CrimeHolder holder, int position) {
-                Crime crime = mCrimes.get(position);
-                holder.mTitleTextView.setText(crime.getmTitle());
-            }
-
-            @Override
-            public int getItemCount() {
-                return mCrimes.size();
-            }
         }
-
-    }
 
